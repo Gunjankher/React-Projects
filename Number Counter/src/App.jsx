4878c19a27f5aafb969b59c655dce4ae575303ad
Gunjankher <gkhercode@gmail.com>
2024-02-22 17:06:59 +0530

    number Counter with react

---
+++ Number Counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(1)
+
+  const addValue = ()=>{
+    counter = counter+1
+    setCounter(counter)
+    console.log(counter);
+  } 
+
+  const removeValue =()=>{
+
+counter= counter-1
+setCounter(counter)
+console.log(counter);
+  }
+  
+  return (
+    <>
+
+
+     <h1>the hooks project</h1>
+     <h2>Counter {counter}</h2>
+     <button onClick={addValue}> 
+      Add Value</button>
+     <button
+     onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
